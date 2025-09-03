8db1acbb60e628084540175b0a65610180b96b7f
shefket-mustafa <miracle_1@abv.bg>
2025-09-03 22:06:25 +0300

    Post model added to backend service

---
+++ server/models/Post.ts
@@ -0,0 +1,7 @@
+import { Schema } from "mongoose";
+
+const postSchema = new Schema({
+    authorId: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    content: {type: String, required: true, maxLength: 500},
+    likes: [{type: Schema.Types.ObjectId, ref: "User"}]
+})
