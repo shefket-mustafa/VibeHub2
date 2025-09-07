f708b85c2b276944a1a503ffcb0ecdc8b9a89ecc
shefket-mustafa <miracle_1@abv.bg>
2025-09-07 23:16:14 +0300

    responsiveness added to header

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -2,8 +2,14 @@ import { Link } from "react-router";
 import { GrLanguage } from "react-icons/gr";
 import { useUser } from "../hooks/user";
 
+import { useState } from "react";
+import { Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+
 export default function Navbar() {
   const { user, logout } = useUser();
+  const [open, setOpen] = useState(false);
 
   return (
     <header className="border-b border-neutral-800 flex px-10 justify-between items-center bg-orange-500 relative z-10 text-white/90">
@@ -17,7 +23,7 @@ export default function Navbar() {
             VibeHub
           </Link>
         </div>
-
+          {/* Desktop menu */}
         <div className=" max-w-5xl p-4 flex  items-center gap-6">
           {user && (
             <Link to="/feed" className="hover:underline">
@@ -31,6 +37,70 @@ export default function Navbar() {
           )}
         </div>
       </div>
+
+      
+     {/* Burger button */}
+     <IconButton onClick={() => setOpen(true)} sx={{ color: "white" }}>
+        <MenuIcon />
+      </IconButton>
+
+      {/* Top drawer (full width) */}
+      <Drawer
+        anchor="top"
+        open={open}
+        onClose={() => setOpen(false)}
+        PaperProps={{
+          sx: {
+            height: "30vh",             
+            backgroundColor: "#f97316",   
+            color: "white",
+          },
+        }}
+      >
+       <Box sx={{ display: "flex", justifyContent: "flex-end", }}>
+    <IconButton onClick={() => setOpen(false)} sx={{ color: "white" }}>
+      <CloseIcon />
+    </IconButton>
+  </Box>
+
+  <List sx={{ px: 4 }}>
+    <ListItem disablePadding>
+      <ListItemButton
+        component={Link}
+        to="/"
+        onClick={() => setOpen(false)}
+        
+      >
+        <ListItemText primary="Home" primaryTypographyProps={{ fontSize: "1.25rem" }} />
+      </ListItemButton>
+    </ListItem>
+
+    <ListItem disablePadding>
+      <ListItemButton
+        component={Link}
+        to="/feed"
+        onClick={() => setOpen(false)}
+        
+      >
+        <ListItemText primary="Feed" primaryTypographyProps={{ fontSize: "1.25rem" }} />
+      </ListItemButton>
+    </ListItem>
+
+    <ListItem disablePadding>
+      <ListItemButton
+        component={Link}
+        to="/profile"
+        onClick={() => setOpen(false)}
+        
+      >
+        <ListItemText primary="Profile" primaryTypographyProps={{ fontSize: "1.25rem" }} />
+      </ListItemButton>
+    </ListItem>
+  </List>
+</Drawer>
+     
+
+
 
       <div className="max-w-5xl p-4 flex items-center gap-6">
         <GrLanguage className="cursor-pointer" />
