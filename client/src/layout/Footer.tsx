c8339928c1034902896b99e636658ea7d527cd93
shefket-mustafa <miracle_1@abv.bg>
2025-09-11 14:40:22 +0300

    Socials icons reworked

--- client/src/layout/Footer.tsx
+++ client/src/layout/Footer.tsx
@@ -1,41 +1,54 @@
+import { Link } from "react-router";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+
 // src/components/layout/Footer.tsx
 export default function Footer() {
     return (
-      <footer className="bg-black text-white py-6 mt-10">
+      <footer className="bg-black text-white py-6 mt-10 ">
         <div className="max-w-7xl mx-auto px-4 flex flex-col  md:flex-row items-center justify-between gap-4">
           {/* Logo / Brand */}
           <span className="text-lg font-semibold text-white">VibeHub</span>
   
           {/* Links */}
-          <nav className="flex gap-6 text-sm">
-            <a href="/about" className="hover:text-white transition-colors">
+          <nav className="flex gap-6 text-sm z-10">
+            <Link to="/about" className="hover:text-white transition-colors">
               About
-            </a>
-            <a href="/features" className="hover:text-white transition-colors">
+            </Link>
+            <Link to="/features" className="hover:text-white transition-colors">
               Features
-            </a>
-            <a href="/contact" className="hover:text-white transition-colors">
+            </Link>
+            <Link to="/contact" className="hover:text-white transition-colors">
               Contact
-            </a>
+            </Link>
           </nav>
   
           {/* Socials */}
-          <div className="flex gap-4">
+          <div className="flex gap-4 z-10">
+          <a
+              href="https://github.com/shefket-mustafa/VibeHub2/tree/main"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-white transition-colors"
+            >
+              <FaGithub />
+            </a>
             <a
               href="https://twitter.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-white transition-colors"
+              className="hover:bg-blue-600 transition-colors"
             >
-              <i className="fab fa-twitter"></i>
+              <FaLinkedin />
             </a>
             <a
-              href="https://github.com"
+              href="https://www.instagram.com/shefket_sum/"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-white transition-colors"
+              className="hover:bg-gradient-to-tr hover:from-yellow-400 hover:via-pink-500 hover:to-purple-600  transition-colors"
             >
-              <i className="fab fa-github"></i>
+              <IoLogoInstagram />
             </a>
           </div>
         </div>
