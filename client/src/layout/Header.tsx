fd0c02e26dae196a54270c55c6c057c844d3261e
shefket-mustafa <miracle_1@abv.bg>
2025-10-25 00:25:29 +0300

    ForgottenPassword and Header translated

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -1,18 +1,20 @@
 import { Link } from "react-router";
 import { GrLanguage } from "react-icons/gr";
 import { useUser } from "../hooks/user";
-
+import i18n from "../i18n"
 import { useState } from "react";
 import { Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import MenuIcon from "@mui/icons-material/Menu";
+import {useTranslation} from "react-i18next"
 
 export default function Navbar() {
   const { user, logout } = useUser();
   const [open, setOpen] = useState(false);
+  const {t} = useTranslation();
 
   return (
-    <header className="border-b border-neutral-800 flex px-10 justify-between items-center bg-orange-500 relative z-10 text-white/90">
+    <header className="border-b border-neutral-800 flex px-10 justify-between items-center bg-orange-500 relative z-2000 text-white/90">
       <div className="flex gap-15">
         <div className="flex items-center">
           <Link
@@ -27,12 +29,12 @@ export default function Navbar() {
         <div className=" max-w-5xl p-4 hidden md:flex  items-center gap-6">
           {user && (
             <Link to="/feed" className="hover:underline">
-              Feed
+              {t("navbar.feed")}
             </Link>
           )}
           {user && (
             <Link to="/profile" className="hover:underline">
-              Profile
+              {t("navbar.profile")}
             </Link>
           )}
         </div>
@@ -130,9 +132,38 @@ export default function Navbar() {
      
 
 
-
       <div className="max-w-5xl p-4 flex items-center gap-4">
-        <GrLanguage className="cursor-pointer text-lg" />
+          {/* Language toggle button */}
+          <div className="relative group">
+  <GrLanguage className="cursor-pointer text-lg text-white" />
+
+  {/* Dropdown */}
+  <div
+    className="
+      absolute right-0 top-full mt-0
+      w-28 bg-neutral-800 border border-neutral-600 rounded-xl shadow-xl
+      opacity-0
+      group-hover:opacity-100
+      transition duration-200 ease-out
+      
+    "
+  >
+    <button
+      onClick={() => i18n.changeLanguage('en')}
+      className="block w-full text-left px-3 py-2 cursor-pointer text-sm text-white hover:bg-neutral-700 rounded-t-xl"
+    >
+      🇬🇧 EN
+    </button>
+    <button
+      onClick={() => i18n.changeLanguage('bg')}
+      className="block w-full text-left px-3 py-2 text-sm cursor-pointer text-white hover:bg-neutral-700 rounded-b-xl"
+    >
+      🇧🇬 BG
+    </button>
+  </div>
+</div>
+
+
 
         {/* Burger button */}
         <div className="md:hidden">
